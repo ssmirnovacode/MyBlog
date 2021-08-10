0defdea72ba55cb8638193cd5dd7470dcab66133
ssmirnova <svetlana.smirnova213@gmail.com>
2021-08-10 16:31:17 +0200

    Posts fetching from DB enabled

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const Post = require('../models/Post');
 
 exports.getIndex = (req,res,next) => {
-    res.render('index', {
-        pageTitle: 'Home'
-    })
+    Post.find()
+        .then(posts => {
+            res.render('index', {
+                pageTitle: 'Home',
+                posts: posts
+            })
+        })
+        .catch(err => console.log(err));
+};
+
+exports.getPosts = (req,res,next) => {
+    
 }

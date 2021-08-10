8f83c335ee99aae1e76791d6a30702fb17136d61
ssmirnova <svetlana.smirnova213@gmail.com>
2021-08-10 16:20:02 +0200

    User and Post models drafted

---
+++ models/Post.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    createdAt: { 
+        type: Date, 
+        default: Date.now 
+    },
+    updatedAt: {
+        type: Date
+    }
+});
+
+module.exports = mongoose.model('Post', postSchema);

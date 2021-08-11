6abc0b1fb2f0b161dd5464f2f4c82e24e02f9313
ssmirnova <svetlana.smirnova213@gmail.com>
2021-08-11 11:34:58 +0200

    User created and saved in req, my-posts enabled

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const path =  require('path');
 const mongoose = require('mongoose');
 
+const User = require('./models/User');
+
 const mainRoutes = require('./routes/mainRoutes');
 
 const MONGODB_URI = 'mongodb+srv://svetlana:node-mongo21@cluster0.rkavg.mongodb.net/blog';
@@ -13,6 +15,15 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req,res,next) => {
+    User.findOne({ _id: '6113964e653b494d96a97a85'})
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err));
+})
 
 app.use(mainRoutes);
 

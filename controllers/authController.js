2a01d16d0a722b7be2ea96a5ac7052feca3995bd
ssmirnova <svetlana.smirnova213@gmail.com>
2021-08-12 12:04:31 +0200

    Validation, errorMsg and old input added to signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,10 +2,28 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator/check');
 
+const getFlashMessage = (req, msgName) => {
+    let message = req.flash(msgName); // 'error' is the key we set up in postLogin
+    if (message.length > 0) {
+        message = message[0]; // message in flash is a part of array  
+    }
+    else {
+        message = null;
+    }
+    return message;
+}
+
 exports.getSignup = (req,res,next) => {
     res.render('signup', {
         pageTitle: 'Sign up',
-        path: '/signup'
+        path: '/signup',
+        errorMessage: '', 
+        oldValues: {
+            name: '',
+            email: '', 
+            password: '',
+            confirmPassword: ''
+        }
     })
 };
 
@@ -13,18 +31,17 @@ exports.postSignup = (req,res,next) => {
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {       
         return res.status(422).render('signup', {
           path: '/signup',
           pageTitle: 'Sign up',
-          errorMessage: `${errors.array()[0].msg}`, // : ${errors.array()[0].param}
-          oldInput: {
-            email: email, 
-            password: password
-          },
-          validationErrors: errors.array()
+          errorMessage: errors.array()[0].msg, // : ${errors.array()[0].param}
+          oldValues: {
+            name, email, password, confirmPassword
+          }
         });
       }
     bcrypt.hash(password, 12)

375be620ca823483ed115d64171f2dc41644b594
ssmirnova <svetlana.smirnova213@gmail.com>
2021-08-19 11:13:45 +0200

    Like icon enabled in post-details

--- models/Comment.js
+++ models/Comment.js
@@ -19,7 +19,24 @@ const commentSchema = new Schema({
     comment: {
         type: String,
         required: true
-    }
+    },
+/*     replies: [
+        {
+            userId: {
+                type: Schema.Types.ObjectId,
+                required: true,
+                ref: 'User'
+            },
+            reply: {
+                type: String,
+                required: true
+            },
+            createdAt: { 
+                type: Date, 
+                default: Date.now 
+            }
+        }
+    ] */
 });
 
 module.exports = mongoose.model('Comment', commentSchema);

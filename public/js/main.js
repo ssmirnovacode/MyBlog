ec27841706503c464a7518da3cf567f7b294bda1
ssmirnova <svetlana.smirnova213@gmail.com>
2021-08-19 16:43:00 +0200

    Dates formatted, pending time zone diff

--- public/js/main.js
+++ public/js/main.js
@@ -22,3 +22,18 @@ try {
   });
 }
 catch(err){}
+
+function convertDate(str) {
+  const arr = str.slice(0,-5).split('T');
+  arr[0] = arr[0].split('-').reverse().join('-');
+  return arr.join(' at ');
+}
+//console.log(convertDate('2021-08-19T14:12:23.308Z'))
+const str = '2021-08-19T14:12:23.308Z';
+const res = str.slice(0,-5).split('T').map(item => {
+  if (item.indexOf('-') > 0) {
+    item = item.split('-').reverse().join('-');
+  }
+  return item;
+}).join(' at ');
+console.log(res);
